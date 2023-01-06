735094cb44f5e502d70c6cad4d5d4b4d494fb8a3
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2023-01-06 09:15:34 +0530

    Create pattern1.1.cpp

---
+++ pattern1.1.cpp
@@ -0,0 +1,43 @@
+/*   * * * * *
+     * * * * * 
+     * * * * *
+      
+     1 1 1 1 1
+     2 2 2 2 2
+     3 3 3 3 3
+     
+     1 2 3 4 5
+     1 2 3 4 5
+     1 2 3 4 5
+     
+     */
+     for(int i=1;i<=n;i++)
+     {
+     for(int j=1;j<=n;j++)
+     {
+      cout<<"* ";
+      }
+      cout<<endl;
+      }
+      cout<<endl;
+      
+      for(int i=1;i<=n;i++)
+      {
+       for(int j=1;i<=n;j++)
+       {
+       cout<<i<<" ";
+       }
+       cout<<endl;
+       }
+       }
+       cout<<endl;
+       for(int i=1;i<n;i++)
+       {
+       for(int j=1;j<=n;j++)
+       {
+        cout<<j<<" ";
+        }
+        cout<<endl;
+        }
+     }
+     cout<<endl;
